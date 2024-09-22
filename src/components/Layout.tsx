d70448ed070870d4d9987dbedfce88ccab87ed98
Luka Shalamberidze <lkshalamberidze@gmail.com>
2024-09-22 12:31:53 +0400

    change toaster

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -19,7 +19,7 @@ const Layout = () => {
     <ShoppingCartProvider>
       <Preloader />
       <ScrollToTop />
-      <Toaster />
+      <Toaster richColors theme="light" />
       <Header />
       <main className="px-3 md:px-7 py-16 md:py-20 w-full">
         <Outlet />
